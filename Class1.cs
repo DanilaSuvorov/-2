3ce17db021c7291806c401f38cacfacbdf5a688c
MaxVonPlatzer <64086514+MaxVonPlatzer@users.noreply.github.com>
2020-11-06 16:27:42 +0300

    брух
    это самое

---
+++ Class1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace многоугольники
+{
+    public abstract class Versch
+    {
+        public Versch(int y1, int x1)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+
+        }
+        public abstract void paintshit(Graphics paper);
+
+        public int x1 { get; set; }
+        public int y1 { get; set; }
+
+        public int SetX { set { x1 = value; } get { return x1; } }
+        public int SetY { set { y1 = value; } get { return y1; } }
+
+        public static int R { set; get; }
+        public static Color col { set; get; }
+        abstract public bool check(int x, int y);
+    }
+    class square : Versch
+
+    {
+        public square(int y1, int x1) : base(y1, x1)
+        {
+
+        }
+
+
+
+        public override bool check(int x, int y)
+        {
+            if (x > x1 - R / 2 && x < x1 + R / 2)
+            {
+                if (y > y1 - R / 2 && y < y1 + R / 2)
+                    return true;
+            }
+            return false;
+
+        }
+        public override void paintshit(Graphics paper)
+        {
+            SolidBrush brush = new SolidBrush(Color.Red);
+            paper.FillRectangle(brush, y1-R/2, x1-R/2, R, R);
+
+
+
+
+        }
+
+    }
+    class circle : Versch
+    {
+        public circle(int y1, int x1) : base(y1, x1)
+        {
+
+        }
+
+
+
+        public override bool check(int x, int y)
+        {
+            if ((x - x1) * (x - x1) + (y - y1) * (y - y1) <= R * R / 4)
+                return true;
+            return false;
+
+        }
+        public override void paintshit(Graphics paper)
+        {
+            SolidBrush brush = new SolidBrush(Color.Red);
+            paper.FillEllipse(brush, y1-R/2, x1-R/2, R,  R);
+
+        }
+
+
+    }
+    class triangle : Versch
+    {
+        public triangle(int y1, int x1) : base(y1, x1)
+        {
+
+        }
+        public override bool check(int x, int y)
+        {
+            if ((x - x1) * (x - x1) + (y - y1) * (y - y1) <= R * R / 4)
+                return true;
+            return false;
+
+        }
+        public override void paintshit(Graphics paper)
+        {
+            SolidBrush brush = new SolidBrush(Color.Red);
+            Point point1 = new Point(y1, x1 - R / 2);
+            Point point2 = new Point(y1 - R / 2, x1 + R / 2);
+            Point point3 = new Point(y1 + R / 2, x1 + R / 2);
+            Point[] coords = { point1, point2, point3 };
+
+
+            paper.FillPolygon(brush, coords);
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace многоугольники
+{
+    public partial class Form1 : Form
+    {
+
+        int x, y;
+        Versch shaper;
+        int shape = 1;
+        int deltax;
+        int deltay;
+        bool existance = false;
+        bool drag;
+        
+        static Form1()
+        {
+
+
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            deltax = 0;
+            deltay = 0;
+            drag = false;
+            Versch shaper;
+        }
+
+        private void КругToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 1;
+            
+        }
+
+        private void КвадратToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 2;
+            
+
+        }
+
+
+
+        private void ТреугольникToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            shape = 3;
+            
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(existance && shaper.check(e.X,e.Y))
+            {
+                if (e.Button== MouseButtons.Left)
+                {
+                    deltax = e.X - shaper.SetX;
+                    deltay = e.Y - shaper.SetY;
+                    drag = true;
+                }
+                if(e.Button == MouseButtons.Right)
+                {
+                    //null
+
+                }
+            }
+            else
+            {
+                existance = true;
+                switch (shape)
+                {
+                    case 1: shaper = new circle(e.X, e.Y);break;
+                    case 2: shaper = new square(e.X, e.Y); break;
+                    case 3: shaper = new triangle(e.X, e.Y); break;
+
+
+                }
+
+            }
+            x = e.X;
+            y = e.Y;
+            
+            this.Invalidate();
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(drag)
+            {
+                shaper.SetX = e.X - deltax;
+                shaper.SetY = e.Y - deltay;
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DoubleBuffered = true;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            shaper.paintshit(e.Graphics);
+
+
+
+        }
+    }
+}
